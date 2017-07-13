c302cd8a50faadfc21bf078a38d5cf1028548e4a
jimmy <jimmy_yeh@alphanetworks.com>
2017-07-13 13:10:19 +0800

    add ico/rc files

--- avrelay/version.h
+++ avrelay/version.h
@@ -1,6 +1,11 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define VERSION "1.0.0.1"
+#define vMAJOR 1
+#define vMINOR 0
+#define vBUILD 0
+#define vPATCH 2
+#define VERSION "1.0.0.2"
+                
 
 #endif
